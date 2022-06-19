444331d7513866545b58f8d3dbab8abc58b1036d
Swan BENOIT <swan.benoit@gmail.com>
2022-06-19 14:24:01 +0200

    adjust spacing

--- lib/container/notes_screen.dart
+++ lib/container/notes_screen.dart
@@ -11,23 +11,44 @@ class NotesScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(backgroundColor: Colors.green),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("Mes Notes", style: TextStyle(fontSize: 30)),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: "Titre",
+            const Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 8.0),
+              child: Text("Mes Notes", style: TextStyle(fontSize: 30)),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Titre",
+                ),
               ),
             ),
-            const TextField(
-              minLines: 4,
-              maxLines: 6,
-              keyboardType: TextInputType.multiline,
-              decoration: InputDecoration(hintText: "Contenu"),
+            const Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 8.0),
+              child: TextField(
+                minLines: 4,
+                maxLines: 6,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(hintText: "Contenu"),
+              ),
             ),
-            ElevatedButton(
-                onPressed: takePhoto, child: const Text("Ajouter un photo")),
-            ElevatedButton(
-                onPressed: addNote, child: const Text("Ajouter ma note"))
+            Padding(
+              padding:
+                  const EdgeInsets.only(right: 8, left: 8, top: 16, bottom: 8),
+              child: ElevatedButton(
+                  onPressed: takePhoto, child: const Text("Ajouter un photo")),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8, left: 8, top: 8),
+              child: SizedBox(
+                width: 200.0,
+                height: 50.0,
+                child: ElevatedButton(
+                    onPressed: addNote, child: const Text("Ajouter ma note")),
+              ),
+            )
           ],
         ));
   }
