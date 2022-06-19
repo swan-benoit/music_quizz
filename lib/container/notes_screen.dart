37f442b5da4767f28684dd4cdd43a4a8fc8d4149
Swan BENOIT <swan.benoit@gmail.com>
2022-06-19 14:08:23 +0200

    add note form and button

--- lib/container/notes_screen.dart
+++ lib/container/notes_screen.dart
@@ -9,7 +9,36 @@ class NotesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.green),
-    );
+        appBar: AppBar(backgroundColor: Colors.green),
+        body: Column(
+          children: [
+            const Text("Mes Notes", style: TextStyle(fontSize: 30)),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: "Titre",
+              ),
+            ),
+            const TextField(
+              minLines: 4,
+              maxLines: 6,
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(hintText: "Contenu"),
+            ),
+            ElevatedButton(
+                onPressed: takePhoto, child: const Text("Ajouter un photo")),
+            ElevatedButton(
+                onPressed: addNote, child: const Text("Ajouter ma note"))
+          ],
+        ));
+  }
+
+  void takePhoto() {
+    print('take photo');
+    //  TODO implement me
+  }
+
+  void addNote() {
+    print('add note');
+    //  TODO implement me
   }
 }
