5651184c15f31a974f1b29cdda959754b1f764ff
Swan BENOIT <swan.benoit@gmail.com>
2022-07-05 18:31:16 +0200

    fix routes

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -3,9 +3,11 @@ import 'package:music_quizz/container/Signup.dart';
 import 'package:music_quizz/container/quizz.dart';
 import 'package:music_quizz/container/start_quizz.dart';
 
+import 'container/Login.dart';
+
 Map<String, WidgetBuilder> appRoute = {
-  '/': (context) => QuizzScreen(),
+  '/login': (context) => const QuizzScreen(),
   '/home': (context) => const StartQuizz(),
-  // '/': (context) => const Login(),
+  '/': (context) => const Login(),
   '/sign-up': (context) => const SignIn(),
 };
